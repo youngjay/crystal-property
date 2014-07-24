65b440f7575c58223f46954b549de37e67e4bd3d
yangjie <hiyoungjay@gmail.com>
2014-07-24 10:46:37 +0800

    dd

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    Abstract: require('./lib/abstract'),
+    Plain: require('./lib/plain'),
+    Number: require('./lib/number'),
+    Array: require('./lib/array'),
+    Composite: require('./lib/composite'),
+    TypedArray: require('./lib/typed-array'),
+};
---
+++ test/index.js
@@ -0,0 +1,138 @@
+var Property = require('../')
+var assert = require('assert');
+var ko = require('knockout');
+
+// describe('abstract', function() {
+//     var p;
+//     beforeEach(function() {
+//         p = new Property.Abstract();
+//     })
+
+//     it('should have enabled visible activated', function() {
+//         assert.equal(ko.isObservable(p.enabled), true)
+//         assert.equal(ko.isObservable(p.visible), true)
+//         assert.equal(ko.isObservable(p.activated), true)
+//     })
+// })
+
+describe('plain', function() {
+    var p;
+    beforeEach(function() {
+        p = new Property.Plain();
+    })
+
+    it('should get / set', function() {
+        var v = '123d';
+        p.set(v);
+        assert.equal(p.get(), v)
+    })
+})
+
+describe('number', function() {
+    var p;
+    beforeEach(function() {
+        p = new Property.Number();
+    })
+
+    it('set null when invalid number', function() {
+        var v = '123d';
+        p.set(v);
+        assert.equal(p.get(), null)
+    })
+
+    it('set correct when valid number', function() {
+        var v = '123.12';
+        p.set(v);
+        assert.equal(p.get(), v)
+    })
+})
+
+describe('array', function() {
+    var p;
+    beforeEach(function() {
+        p = new Property.Array();
+    })
+
+    it('should get / set', function() {
+        var v = [1,2];
+        p.set(v);
+        assert.deepEqual(p.get(), v)
+    })
+
+    it('reset can invoke multiple times', function() {
+        var defaultValue = [1,2,3]
+
+        p = new (Property.Array.extend({
+            defaultValue: defaultValue
+        }))
+
+        assert.deepEqual(p.get(), defaultValue)
+
+
+        p.reset();
+
+        p.set([123])
+
+        p.reset();
+
+        assert.deepEqual(p.get(), defaultValue)
+
+
+    })
+})
+
+describe('composite', function() {
+    it('should get / set', function() {
+        var v = '123d';
+
+        var p = new (Property.Composite.extend({
+            mapping: {
+                a: Property.Plain,
+                b: Property.Composite.extend({
+                    mapping: {
+                        c: Property.Plain
+                    }
+                })
+            }
+        }))
+
+        p.set({
+            a: 2,
+            b: {
+                c: 3
+            },
+            do_not_set: 1
+        })
+
+        assert.deepEqual(p.get(), {
+            a: 2,
+            b: {
+                c: 3
+            }
+        })
+
+    })
+})
+
+describe('typed array', function() {
+
+    it('should get / set', function() {
+        var C = Property.Number.extend({
+            get: function() {
+                return this.value() + 1;
+            }
+        })
+
+        var A = Property.TypedArray.extend({
+            mapping: C
+        })
+
+        var a = new A();
+        a.set([1,2])
+
+        assert.deepEqual(a.get(), [2,3])
+
+    })
+
+
+})
